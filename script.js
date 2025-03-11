52be03d527b178ff65c1f93537ddf1816b7a8088
jasmina001 <jorayevajasmina76@gmail.com>
2025-03-11 21:05:14 +0500

    add new section

--- script.js
+++ script.js
@@ -10,12 +10,19 @@ function getCountryInfo() {
         .then(response => response.json())
         .then(data => {
             let countryData = data[0];
+
+            let borders = "Chegaradosh davlatlar yo'q"; 
+            if (countryData.borders) {
+                borders = countryData.borders.join(", ");
+            }
+
             document.getElementById("result").innerHTML = `
                 <p><strong>Davlat:</strong> ${countryData.name.common}</p>
                 <p><strong>Aholisi:</strong> ${countryData.population.toLocaleString()}</p>
                 <p><strong>Region:</strong> ${countryData.region}</p>
                 <p><strong>Poytaxt:</strong> ${countryData.capital ? countryData.capital[0] : "Ma'lumot yo'q"}</p>
                 <p><strong>Kelib chiqishi:</strong> ${countryData.subregion}</p>
+                <p><strong>Chegaradosh davlatlar:</strong> ${borders}</p>
                 <img src="${countryData.flags.png}" alt="Bayroq">
             `;
         })
@@ -24,3 +31,8 @@ function getCountryInfo() {
             console.error("Xatolik:", error);
         });
 }
+
+
+
+
+
