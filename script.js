390586241b39e4d63391d0c5f99dbfd531b22301
jasmina001 <jorayevajasmina76@gmail.com>
2025-04-01 16:18:15 +0500

    add

--- script.js
+++ script.js
@@ -1,53 +1,36 @@
-// function getCountryInfo() {
-//     let country = document.getElementById("countryInput").value.trim();
+function getCountryInfo() {
+    let country = document.getElementById("countryInput").value.trim();
     
-//     if (country === "") {
-//         alert("Iltimos, davlat nomini kiriting!");
-//         return;
-//     }
+    if (country === "") {
+        alert("Iltimos, davlat nomini kiriting!");
+        return;
+    }
     
-//     fetch(`https://restcountries.com/v3.1/name/${country}`)
-//         .then(response => response.json())
-//         .then(data => {
-//             let countryData = data[0];
-
-//             let borders = "Chegaradosh davlatlar yo'q"; 
-//             if (countryData.borders) {
-//                 borders = countryData.borders.join(", ");
-//             }
-
-//             document.getElementById("result").innerHTML = `
-//                 <p><strong>Davlat:</strong> ${countryData.name.common}</p>
-//                 <p><strong>Aholisi:</strong> ${countryData.population.toLocaleString()}</p>
-//                 <p><strong>Region:</strong> ${countryData.region}</p>
-//                 <p><strong>Poytaxt:</strong> ${countryData.capital ? countryData.capital[0] : "Ma'lumot yo'q"}</p>
-//                 <p><strong>Kelib chiqishi:</strong> ${countryData.subregion}</p>
-//                 <p><strong>Chegaradosh davlatlar:</strong> ${borders}</p>
-//                 <img src="${countryData.flags.png}" alt="Bayroq">
-//             `;
-//         })
-//         .catch(error => {
-//             document.getElementById("result").innerHTML = `<p style="color: red;">Xatolik: Davlat topilmadi!</p>`;
-//             console.error("Xatolik:", error);
-//         });
-// }
-
-
-
-
-
-document.addEventListener("DOMContentLoaded", () => {
-    const container = document.getElementById("image-container");
-
-    fetch("https://jsonplaceholder.typicode.com/photos?")
+    fetch(`https://restcountries.com/v3.1/name/${country}`)
         .then(response => response.json())
         .then(data => {
-            data.forEach(photo => {
-                const img = document.createElement("img");
-                img.src = photo.thumbnailUrl;
-                img.alt = photo.title;
-                container.appendChild(img);
-            });
+            let countryData = data[0];
+
+            let borders = "Chegaradosh davlatlar yo'q"; 
+            if (countryData.borders) {
+                borders = countryData.borders.join(", ");
+            }
+
+            document.getElementById("result").innerHTML = `
+                <p><strong>Davlat:</strong> ${countryData.name.common}</p>
+                <p><strong>Aholisi:</strong> ${countryData.population.toLocaleString()}</p>
+                <p><strong>Region:</strong> ${countryData.region}</p>
+                <p><strong>Poytaxt:</strong> ${countryData.capital ? countryData.capital[0] : "Ma'lumot yo'q"}</p>
+                <p><strong>Kelib chiqishi:</strong> ${countryData.subregion}</p>
+                <p><strong>Chegaradosh davlatlar:</strong> ${borders}</p>
+                <img src="${countryData.flags.png}" alt="Bayroq">
+            `;
         })
-        .catch(error => console.error("Xatolik yuz berdi:", error));
-});
+        .catch(error => {
+            document.getElementById("result").innerHTML = `<p style="color: red;">Xatolik: Davlat topilmadi!</p>`;
+            console.error("Xatolik:", error);
+        });
+}
+
+
+
